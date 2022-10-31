6aa7584991a30a33de4b9e8a7d5da89a0ffd5ede
Albazc <albazcano@gmail.com>
2022-11-01 00:53:27 +0100

    modified pokemon options and pokemon picture

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,11 +1,38 @@
 <template>
-  <h3>Pokemon Picture</h3>
+  <div class= "pokemon-container">
+
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+          class="hidden-pokemon"
+          alt="pokemon">
+
+    <!-- <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+          class="fade-in"
+          alt="pokemon"> -->
+  </div>
+ 
 </template>
 
 <script>
 
 </script>
 
-<style>
+<style scoped>
+
+.pokemon-container {
+    height: 200px;
+}
+img {
+    height: 200px;
+    position: absolute;
+    right: 40%;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
 
 </style>
