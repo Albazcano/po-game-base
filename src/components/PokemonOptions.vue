243bdba69eef349f31b13e494044b55f5ddb8066
Albazc <albazcano@gmail.com>
2022-11-01 00:34:58 +0100

    added pokemon page, picture and options, modified app

---
+++ src/components/PokemonOptions.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3>Pokemon Options</h3>
+  </template>
+  
+  <script>
+  
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>Pokemon Picture</h3>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
