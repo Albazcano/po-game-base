1529888a7d5d4c3ae7c31ed5e703fa4767c767fe
Albazc <albazcano@gmail.com>
2022-11-01 01:54:56 +0100

    added pokemon api and modified get pokemon options

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -11,9 +11,13 @@ const getPokemonOptions = () => {
 
     const mixedPokemons = getPokemons().sort( () => Math.random() - 0.5 )
 
-    console.log(mixedPokemons())
+    getPokemonNames(mixedPokemons.splice(0,4) )
 }
 
+const getPokemonNames = ( [a,b,c,d] = [] ) => {
+    console.log( a, b, c, d)
+    
+}
 
 
 
