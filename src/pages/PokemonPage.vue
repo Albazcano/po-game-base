51e8310d8475e6c0064d78e9f6cfd897efee0570
Albazc <albazcano@gmail.com>
2022-11-01 01:41:08 +0100

    added getPokemonOptions and modified pokemonPages

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,7 +1,7 @@
 <template>
    <h1>¿Quién es este Pokemon?</h1>
 
-   <PokemonPicture :pokemonId="4" :showPokemon= "true"/>
+   <PokemonPicture :pokemonId="251" :showPokemon= "true"/>
    <PokemonOptions /> 
 
 
@@ -10,6 +10,10 @@
 <script>
    import PokemonOptions from '../components/PokemonOptions.vue'
    import PokemonPicture from '../components/PokemonPicture.vue'
+
+   import getPokemonOptions from '../helpers/getPokemonOptions'
+
+   console.log(getPokemonOptions())
 
 export default {
     components: { PokemonOptions, PokemonPicture }
