783efba14f67a1177e3d59880ca8032afc72f15d
Albazc <albazcano@gmail.com>
2023-01-25 12:18:51 +0100

    modify PokemonPicture test

---
+++ tests/unit/components/PokemonPicture.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
+import PokemonPicture from '../../../src/components/PokemonPicture.vue'
+
+describe('PokemonPicture component', () => {
+    
+    test('debe de hacer match con el snapshot', () => {
+    
+    })
+})
